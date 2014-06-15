7cee2af5ea3e9e764091ef534ec1d989984ecc07
llv22 <llv22@llv22s-Mac.local>
2014-06-15 09:51:33 +0800

    llv22 - for fibonacci in advance swift - session 404

--- swift/SwiftPlayground.playground/section-1.swift
+++ swift/SwiftPlayground.playground/section-1.swift
@@ -11,6 +11,16 @@ for var l = 1; l < 10; l++
     println(l)
 }
 
+func memoize<T: Hashable, U>( body: ((T)->U, T)->U ) -> (T)->U {
+    var memo = Dictionary<T, U>()
+    return { x in
+        if let q = memo[x] { return q }
+        let r = body(x)
+        memo[x] = r
+        return r
+    }
+}
+
 let fibonacci: (Int)->Double = memoize{
     fibonacci, n in
     n < 2 ? Double(n) : fibonacci(n-2) + fibonacci(n-1)
