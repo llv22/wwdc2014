270a66dd066b2bb131bf5e7bc8f9706158b2acd9
Orlando Ding <xiandao.airs@gmail.com>
2014-07-29 23:15:56 +0800

    llv22 - generic of swift

--- swift/SwiftPlayground.playground/section-1.swift
+++ swift/SwiftPlayground.playground/section-1.swift
@@ -456,3 +456,23 @@ func repeat<ItemType>(item: ItemType, times: Int) -> [ItemType]{
     return result;
 }
 repeat("knock", 4);
+
+enum OptionalValue<T>{
+    case None;
+    case Some(T);
+}
+var possibleInteger: OptionalValue<Int> = .None;
+possibleInteger = .Some(100);
+
+func anyCommonElements <T, U where U: Sequence, U: Sequence, T.GeneratorType.Element: Equatable,
+    T.GeneratorType.Element == U.GeneratorType.Element> (lhs: T, rhs: U) -> Bool {
+        for lhsItem in lhs {
+            for rhsItem in rhs {
+                if lhsItem == rhsItem {
+                    return true;
+                }
+            }
+        }
+        return false;
+}
+anyCommonElements([1, 2, 3], [3]);
