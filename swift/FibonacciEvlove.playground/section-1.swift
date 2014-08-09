b0dc109d08ba259079a7a7f27693120e951fcc02
llv22 <xiandao.airs@gmail.com>
2014-08-09 18:53:37 +0800

    llv22 - for swift of fibonacci recursive of cloure

--- swift/FibonacciEvlove.playground/section-1.swift
+++ swift/FibonacciEvlove.playground/section-1.swift
@@ -12,7 +12,7 @@ fibonacci_nonmemoize(20)
 */
 
 /*
-* 2, recursive with manual memooize
+* 2, recursive with manual memoize
 */
 var fibonacci_manualmem = Dictionary<Int, Double>()
 func fibonacci_mm(n: Int) -> Double{
@@ -25,3 +25,48 @@ func fibonacci_mm(n: Int) -> Double{
 }
 fibonacci_mm(20)
 
+/*
+* 3, recursive with auto memoize for result to avoid type inference
+*/
+func memoizeAuto<T: Hashable, U>( body: (T)-> U ) -> (T)->U {
+    var memo = Dictionary<T, U>();
+    return {
+        x in
+        if let q = memo[x] {return q}
+        let r = body(x)
+        memo[x] = r
+        return r
+    }
+}
+/* error: variable used within its own initial value
+let fibonacciAuto = memoizeAuto{
+    fibonacciAuto, n in
+    n < 2 ? Double(n) : fibonacciAuto(n-1) + fibonacciAuto(n-2)
+}
+*/
+var fibonacciAuto: (Int) -> Double = { Double($0) }
+fibonacciAuto = memoizeAuto {
+    (n : Int) in n < 2 ? Double(n) : fibonacciAuto(n-1) + fibonacciAuto(n-2)
+}
+fibonacciAuto(20)
+
+
+/*
+* 4, recursive with auto memoize for result to avoid type inference
+*/
+func memoizeBodyResult<T: Hashable, U>( body: ((T)->U, T)->U ) -> (T)->U {
+    var memo = Dictionary<T, U>()
+    var result: ((T)->U)!
+    result = {
+        x in
+        if let q = memo[x] {return q}
+        let r = body(result, x)
+        memo[x] = r
+        return r
+    }
+    return result
+}
+let fibonacci = memoizeBodyResult{
+    (fib, x: Int) in x < 2 ? Double(x) : fib(x-1) + fib(x-2)
+}
+fibonacci(20)
