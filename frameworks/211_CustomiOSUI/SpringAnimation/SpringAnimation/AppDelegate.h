4172f6d6f14cb210144c6086cd8935a75165edf7
Orlando Ding <llv23@llv23s-Mac.local>
2014-12-17 18:37:37 +0800

    llv23 - for spring animation with constraint update in UIView

---
+++ frameworks/211_CustomiOSUI/SpringAnimation/SpringAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpringAnimation
+//
+//  Created by llv23 on 12/17/14.
+//  Copyright (c) 2014 llv23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
