2aa3d2a39d74317b5608300ec8da66b7e7de2be1
Orlando Ding <xiandao.airs@gmail.com>
2017-10-07 12:03:54 +0800

    llv23 - for removal of deperated apple watch API

--- watchkit/MyApp/MyApp WatchKit Extension/GlanceController.m
+++ watchkit/MyApp/MyApp WatchKit Extension/GlanceController.m
@@ -11,27 +11,24 @@
 
 @implementation GlanceController
 
-- (instancetype)initWithContext:(id)context {
-    self = [super initWithContext:context];
-    if (self){
-        // Initialize variables here.
-        // Configure interface objects here.
-        NSLog(@"%@ initWithContext", self);
-        CityObject* city = context;
-        
-        [self.cityLabel setText:city.cityName];
-        //see http://www.cnblogs.com/ygm900/archive/2013/05/22/3093941.html
+/**
+ see reference in https://stackoverflow.com/questions/27796088/no-visible-interface-for-wkinterfacecontroller-declares-the-selector-initwit
+ */
+- (void)awakeWithContext:(id)context {
+    // Configure interface objects here. Apple watch API changed, as loading context directly
+    CityObject* city = context;
+    
+    [self.cityLabel setText:city.cityName];
+    //see http://www.cnblogs.com/ygm900/archive/2013/05/22/3093941.html
 //        CLLocationCoordinate2D centerCoordinate = CLLocationCoordinate2DMake(39.915352,116.397105);
-        //see http://tergol.blog.163.com/blog/static/17069502820108205020506/
-        MKCoordinateSpan span;
-        span.latitudeDelta = 0.1;
-        span.longitudeDelta = 0.1;
-        //see http://blog.csdn.net/tangaowen/article/details/6527901
+    //see http://tergol.blog.163.com/blog/static/17069502820108205020506/
+    MKCoordinateSpan span;
+    span.latitudeDelta = 0.1;
+    span.longitudeDelta = 0.1;
+    //see http://blog.csdn.net/tangaowen/article/details/6527901
 //        MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(centerCoordinate, 1000, 1000);
-        MKCoordinateRegion region = MKCoordinateRegionMake(city.cityLocation, span);
-        [self.cityMap setCoordinateRegion:region];
-    }
-    return self;
+    MKCoordinateRegion region = MKCoordinateRegionMake(city.cityLocation, span);
+    [self.cityMap setRegion:region];
 }
 
 @end
